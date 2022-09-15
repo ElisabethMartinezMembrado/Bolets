ee12bb6b88ea6cccfc443b40ce455189aee0300e
Elisabeth <elisabeth.martinez.membrado@gmail.com>
2022-09-15 22:03:49 +0200

    v1.3.0

--- src/Views/Home/Home.tsx
+++ src/Views/Home/Home.tsx
@@ -3,15 +3,19 @@ import React from 'react';
 import { boletTipo } from '../../Utils/tiposDatos';
 import Carta from '../../Componentes/Carta/Carta';
 import './Home.css';
+import Skeleton from '@mui/material/Skeleton';
+import Stack from '@mui/material/Stack';
 
 function Home (){
     const [Bolets, setBolets] = React.useState<boletTipo[]>();
+    const [Loading, setLoading] = React.useState(true);
 
     async function RecibirDatos (){
         const RespuestaApi = await fetch("https://62d4fcf2cd960e45d45ea776.mockapi.io/bolets/")
         const RespuestaApiTxt = await RespuestaApi.text();
         const RespuestaApiParse = JSON.parse(RespuestaApiTxt)
         setBolets(RespuestaApiParse);
+        setLoading(false);
     }
     
     // se usa para que react ejecute la function 1 vez cuando el componente este listo
@@ -21,29 +25,36 @@ function Home (){
 
     return(
         <div id='Body'>
-        <Typography variant='h3' id="Titulo" color="#18A2A8" >Nuestros Bulets</Typography>
-        {Bolets?.map(function(CadaBolet){
-            return(
-                <Carta 
-                    key={CadaBolet.id} 
-                    createdAt={CadaBolet.createdAt} 
-                    name = {CadaBolet.name}
-                    imageUrl = {CadaBolet.imageUrl}
-                    description = {CadaBolet.description}
-                    comestible = {CadaBolet.comestible}
-                    recomendaciones = {CadaBolet.recomendaciones}
-                    calidad = {CadaBolet.calidad}
-                    temporada = {CadaBolet.temporada}
-                    etiquetas = {CadaBolet.etiquetas}
-                    altImagenes = {CadaBolet.altImagenes}
-                    himeni = {CadaBolet.himeni}
-                    anillo = {CadaBolet.anillo}
-                    pie = {CadaBolet.pie}
-                    vulva = {CadaBolet.vulva}
-                    id = {CadaBolet.id}
-                />
-            )
-        })}
+        <Typography variant='h3' id="Titulo" color="#18A2A8" >Nuestros Bolets</Typography>
+        { !Loading ? 
+            Bolets?.map(function(CadaBolet){
+                return(
+                    <Carta 
+                        key={CadaBolet.id} 
+                        createdAt={CadaBolet.createdAt} 
+                        name = {CadaBolet.name}
+                        imageUrl = {CadaBolet.imageUrl}
+                        description = {CadaBolet.description}
+                        comestible = {CadaBolet.comestible}
+                        recomendaciones = {CadaBolet.recomendaciones}
+                        calidad = {CadaBolet.calidad}
+                        temporada = {CadaBolet.temporada}
+                        etiquetas = {CadaBolet.etiquetas}
+                        altImagenes = {CadaBolet.altImagenes}
+                        himeni = {CadaBolet.himeni}
+                        anillo = {CadaBolet.anillo}
+                        pie = {CadaBolet.pie}
+                        vulva = {CadaBolet.vulva}
+                        id = {CadaBolet.id}
+                    />
+                )
+            }) 
+        : (<Stack>
+                
+                
+                <Skeleton className="spinner" animation="wave" variant="rectangular" width={355} height={300} />
+            </Stack>
+            ) }
         </div>
     )
 }
