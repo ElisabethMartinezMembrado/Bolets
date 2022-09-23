ab03d2018f1957fc3a12dcfde6a595655eefef34
Elisabeth <elisabeth.martinez.membrado@gmail.com>
2022-09-23 12:43:23 +0200

    v3.2.0

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import './App.css';
 import { HashRouter, Routes, Route } from 'react-router-dom';
 import Details from './Views/Details/Details';
 import NotFound from './Views/NotFound/NotFound';
-import Navbar from './Componentes/Navbar/Navbar';
+
 
 
 function App() {
@@ -19,7 +19,7 @@ function App() {
           <Route path="*" element ={<NotFound/>}/>
         </Routes>
       </HashRouter>
-      <p>v3.1.0</p>
+      <p>v3.2.0</p>
      
     </div>
   );
